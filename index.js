e361bf6caa71478e49f727e3baeb5430878c7dc4
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-06-18 23:27:41 -0300

    Projeto finalizado

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('oi')
+const ul = document.querySelector('ul')
+
+fetch('data.json').then(res => {
+  res.json().then(data => {
+    data.map(el => {
+      ul.innerHTML += `
+        <li class='data'>
+          <div>
+            <img src='./assets/images/icon-${el.category.toLowerCase()}.svg' alt='${el.category}'/>
+            <p class='${el.category.toLowerCase()}'>${el.category}</p>
+          </div>
+          <div>
+            <p class='pointing'>${el.score}</p> 
+            <p>/</p>
+            <p>100</p>
+          </div>
+        </li>
+      `
+    })
+  })
+}). catch(e => console.log(e))
